7b10b51894a40076073264bf82a9caf3df2b76c6
Maxime Baratange <mbaratange@gmail.com>
2023-11-26 16:50:15 +0100

    Faire la route login

--- app.js
+++ app.js
@@ -1,6 +1,16 @@
 const express = require('express');
+const mongoose = require('mongoose');
+
+const stuffRoutes = require('./routes/stuff');
+const userRoutes = require('./routes/user');
 
 const app = express();
+
+mongoose.connect('mongodb+srv://Tex-Eric:MartinSarol@clustermaxima.dw4es4q.mongodb.net/',
+  { useNewUrlParser: true,
+    useUnifiedTopology: true })
+  .then(() => console.log('Connexion à MongoDB réussie !'))
+  .catch(() => console.log('Connexion à MongoDB échouée !'));
 
 app.use(express.json());
 
@@ -11,35 +21,8 @@ app.use((req, res, next) => {
    next();
 });
 
-app.post('/api/stuff', (req, res, next) => {
-   console.log(req.body);
-   res.status(201).json({
-     message: 'Objet créé !'
-   });
-});
-
-app.get('/api/stuff', (req, res, next) => {
-   const stuff = [
-     {
-       _id: 'oeihfzeoi',
-       title: 'Mon premier objet',
-       description: 'Les infos de mon premier objet',
-       imageUrl: 'https://cdn.pixabay.com/photo/2019/06/11/18/56/camera-4267692_1280.jpg',
-       price: 4900,
-       userId: 'qsomihvqios',
-     },
-     {
-       _id: 'oeihfzeomoihi',
-       title: 'Mon deuxième objet',
-       description: 'Les infos de mon deuxième objet',
-       imageUrl: 'https://cdn.pixabay.com/photo/2019/06/11/18/56/camera-4267692_1280.jpg',
-       price: 2900,
-       userId: 'qsomihvqios',
-     },
-   ];
-   res.status(200).json(stuff);
-   next();
-});
+app.use('/api/stuff', stuffRoutes);
+app.use('/api/auth', userRoutes);
 
 app.use((req, res, next) => {
   console.log('Réponse envoyée avec succès !');
