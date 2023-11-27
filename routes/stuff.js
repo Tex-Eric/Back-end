250001e5681470fdde60d54df18bccb6a43e2a4e
Maxime Baratange <mbaratange@gmail.com>
2023-11-27 15:35:58 +0100

    Tous les exercices sont terminés !

--- routes/stuff.js
+++ routes/stuff.js
@@ -1,12 +1,14 @@
 const express = require('express');
 const router = express.Router();
+const auth = require('../middleware/auth');
+const multer = require('../middleware/multer-config')
 
 const stuffCtrl = require('../controllers/stuff');
 
 router.get('/', stuffCtrl.getAllStuff);
-router.post('/', stuffCtrl.createThing);
+router.post('/', auth, multer, stuffCtrl.createThing);
 router.get('/:id', stuffCtrl.getOneThing);
-router.put('/:id', stuffCtrl.modifyThing);
-router.delete('/:id', stuffCtrl.deleteThing);
+router.put('/:id', auth, multer, stuffCtrl.modifyThing);
+router.delete('/:id', auth, stuffCtrl.deleteThing);
 
 module.exports = router;
